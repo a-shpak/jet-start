0609a819feb1afeb87f45d0408a3e1edd7ba8fb5
AndreyShpakIT <andrey.shpak.it@gmail.com>
2021-10-01 10:46:08 +0300

    fixed start path

--- sources/myapp.js
+++ sources/myapp.js
@@ -8,7 +8,7 @@ export default class MyApp extends JetApp{
 			version : VERSION,
 			router 	: BUILD_AS_MODULE ? EmptyRouter : HashRouter,
 			debug 	: true,
-			start 	: "/top/start"
+			start 	: "/top/data"
 		};
 
 		super({ ...defaults, ...config });
